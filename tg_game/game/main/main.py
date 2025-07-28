355188cca965ae7e261f56305990d476a4a6f493
Anterro <geoleroworkpost@yahoo.com>
2025-07-29 00:59:05 +0200

    Створено скелет, додано купу коментарів, додано базу даних і таблицю в ній

--- tg_game/game/main/main.py
+++ tg_game/game/main/main.py
@@ -1,14 +1,66 @@
+#закоментовані бібліотеки не видаляти, вони треба будуть. Розкоментовувати по мірі необхідності
+#import random
+import sqlite3
 import telebot
+from telebot import types #Для кнопочок
+
+
+conn = sqlite3.connect("main_DB.sqlite3") # ініціалізація БД
+c = conn.cursor() # створюємо курсор
+
 
 bot = telebot.TeleBot("8486071865:AAHEI1dUX63DUZIqx9PDde6wQBLDnbqhuyA")
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+@bot.message_handler(commands=["start"]) # commands=[""] реагує тільки на вручну написані команди типу "/start"
+def game_start(message): # основний набір кнопок для швидкої навігації (бажано не рухати ніхуя)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Магазин")
+    btn2 = types.KeyboardButton("Статистика")
+    btn3 = types.KeyboardButton("Землі")
+    btn4 = types.KeyboardButton("Будівлі")
+    markup.add(btn1, btn2, btn3, btn4)
+    bot.send_message(message.from_user.id, "Що робимо?",reply_markup=markup)
+    
+"""
+Потрібно подумати як організувати ці структури
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+@bot.message_handler(func=lambda msg: msg.text == "Статистика")
+def stat_handler(message): 
+	pass  
 
+@bot.message_handler(func=lambda msg: msg.text == "Землі") 
+def fields_handler(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton("Ліси", callback_data="forest_categori")
+    btn2 = types.InlineKeyboardButton("Поля", callback_data = "field_categori")
+    btn3 = types.InlineKeyboardButton("Озера", callback_data = "lakes_categori")
+    markup.add(btn1, btn2, btn3)
+    bot.send_message(message.chat.id, "Що робимо?", reply_markup=markup)
+
+@bot.message_handler(func=lambda msg: msg.text == "Будівлі")
+def houses_handler(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton("Маєтки", callback_data="home_categori")
+    btn2 = types.InlineKeyboardButton("Хліви", callback_data = "animal_home_categori")
+    markup.add(btn1, btn2)
+    bot.send_message(message.chat.id, "Що робимо?", reply_markup=markup)
+
+"""
+
+@bot.message_handler(func=lambda msg: msg.text == "Магазин") # обробка з Keybord кнопок
+def shop_handler(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton("Автомобілі", callback_data="car_categori")
+    btn2 = types.InlineKeyboardButton("Поля", callback_data = "field_categori")
+    markup.add(btn1, btn2)
+    bot.send_message(message.chat.id, "Що робимо?", reply_markup=markup)
+    
+@bot.callback_query_handler(func=lambda call: call.data == "car_categori") # для Inline кнопочок використовуєм декоратор callback_query_handler
+def car_sell_list(call): # обробка кнопочок Inline робиться через call в передаваному аргументі
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton("Audi", callback_data="buy_audi")
+    btn2 = types.InlineKeyboardButton("BMW", callback_data="buy_BMW")
+    markup.add(btn1, btn2)
+    bot.send_message(call.message.chat.id, "Оберіть авто:", reply_markup=markup)
+    
 bot.polling()
-# повертає те що ти йому написав (безжально спизжений код)
